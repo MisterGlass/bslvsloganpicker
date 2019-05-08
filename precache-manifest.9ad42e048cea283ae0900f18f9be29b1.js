d7cda184c31e74b5f52ca363cef0c5e562fcc7f8
Yair Silbermintz <yair.silbermintz@aon.com>
2019-05-08 10:33:33 -0400

    Updates

---
+++ precache-manifest.9ad42e048cea283ae0900f18f9be29b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7470e6929a846b683369a460924cb63f",
+    "url": "/bslvsloganpicker/index.html"
+  },
+  {
+    "revision": "d9a1aa8598e6e451414d",
+    "url": "/bslvsloganpicker/static/css/main.7c0ead62.chunk.css"
+  },
+  {
+    "revision": "83e6174aa5919a4b78ce",
+    "url": "/bslvsloganpicker/static/js/2.4e629e7f.chunk.js"
+  },
+  {
+    "revision": "d9a1aa8598e6e451414d",
+    "url": "/bslvsloganpicker/static/js/main.ca078928.chunk.js"
+  },
+  {
+    "revision": "031c43d12b260618629c",
+    "url": "/bslvsloganpicker/static/js/runtime~main.666221a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/bslvsloganpicker/precache-manifest.bf82eaa8e20573f63b832ee02de5a7ce.js"
+  "/bslvsloganpicker/precache-manifest.9ad42e048cea283ae0900f18f9be29b1.js"
 );
 
 self.addEventListener('message', (event) => {
