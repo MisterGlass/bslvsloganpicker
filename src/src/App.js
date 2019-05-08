e9536c58b53dfc013bbc632d0d3203dfd78d32cc
Yair Silbermintz <yair.silbermintz@aon.com>
2019-05-08 09:55:59 -0400

    updates to code

--- src/src/App.js
+++ src/src/App.js
@@ -7,15 +7,68 @@ import './App.css';
 
 import slogans from './sloganlist.json';
 
-function Slogan(props) {
-    return (
-        <div className="slogan">
-            <p>{props.text}</p>
-            <button className="favbtn" onClick={props.favslogan}>
-                Favorite
-            </button>
-        </div>
-    );
+class Slogan extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.id,
+            text: props.text,
+            favfunc: props.favslogan,
+            unfavfunc: props.unfavslogan,
+            hidden: false,
+            favorited: props.favorited,
+        }
+        if(props.favorited) {console.log('gotcha')};
+    };
+
+    favslogan = () => {
+        this.state.favfunc();
+    }
+
+    unfavslogan = () => {
+        this.state.unfavfunc();
+    }
+
+    hideslogan = () => {
+        this.setState({hidden: true});
+    }
+
+    unhideslogan = () => {
+        this.setState({hidden: false});
+    }
+    
+    render() {
+        if (this.state.hidden) {
+            return (
+                <div className="slogan hidden">
+                    <button className="unhidebtn" onClick={this.unhideslogan}>
+                        Show
+                    </button>
+                </div>
+            );
+        } else if (this.state.favorited) {
+            return (
+                <div className="slogan favorite">
+                    <p>{this.state.text}</p>
+                    <button className="favbtn" onClick={this.unfavslogan}>
+                        Unfavorite
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="slogan">
+                    <p>{this.state.text}</p>
+                    <button className="favbtn" onClick={this.favslogan}>
+                        Favorite
+                    </button>
+                    <button className="hidebtn" onClick={this.hideslogan}>
+                        Hide
+                    </button>
+                </div>
+            );
+        }
+    }
 }
 
 function Favorite(props) {
@@ -31,18 +84,33 @@ function Favorite(props) {
 
 class Main extends React.Component {
     constructor(props) {
+        var mainslogans = slogans.slice();
+        mainslogans.forEach(function(slogan) {
+            slogan.favorited = false;
+        });
+
         super(props);
         this.state = {
             favorites: [],
             user: null,
             submitted: false,
+            slogans: mainslogans,
         };
     }
 
     favSlogan(key, slogan) {
         const favorites = this.state.favorites.slice();
         favorites.push(slogan);
-        this.setState({favorites: favorites});
+
+        const mainslogans = this.state.slogans.slice();
+        for (var i = 0; i < mainslogans.length; i++) { 
+            if (mainslogans[i].id === slogan.id) {
+                mainslogans[i].favorited = true;
+                break;
+            }
+        }
+
+        this.setState({favorites: favorites, slogans: mainslogans});
     }
 
     unfavSlogan(key, slogan) {
@@ -53,12 +121,28 @@ class Main extends React.Component {
                 break;
             }
         }
-        this.setState({favorites: favorites});
+
+        const mainslogans = this.state.slogans.slice();
+        for (i = 0; i < mainslogans.length; i++) { 
+            if (mainslogans[i].id === slogan.id) {
+                mainslogans[i].favorited = false;
+                break;
+            }
+        }
+
+        this.setState({favorites: favorites, slogans: mainslogans});
     }
 
     renderSlogan(key, slogan) {
         return (
-            <Slogan key={key} text={slogan.slogan} favslogan={() => this.favSlogan(key, slogan)} />
+            <Slogan 
+                key={key}
+                id={slogan.id}
+                text={slogan.slogan}
+                favslogan={() => this.favSlogan(key, slogan)}
+                unfavslogan={() => this.unfavSlogan(key, slogan)}
+                favorited={slogan.favorited}
+            />
         )
     }
 
@@ -89,7 +173,6 @@ class Main extends React.Component {
             var favids = this.state.favorites.map((slogan) => {
                 return slogan.id;
             });
-            console.log(JSON.stringify(favids));
 
             const formData = new FormData()
             formData.append(usernamefield, this.state.user.email)
@@ -114,7 +197,7 @@ class Main extends React.Component {
         if (this.state.user == null) {
             return (
                 <div style={{padding: "20px"}}>
-                    Please log in with your BSidesLV account to continue<br />
+                    Please log in with your BSidesLV account to continue<br /><br />
                     <GoogleLogin
                         clientId="693738722966-m2uhbrdhrsb1el7u0uiomj1lmov1bbrr.apps.googleusercontent.com"
                         buttonText="Login"
@@ -140,7 +223,7 @@ class Main extends React.Component {
                         Note: you can add however many slogans you like to your favorites, but if you have more than 10 you will need to remove them from your list before submitting.<br />
                     </p>
                     <div className="slogans">
-                        {slogans.map((slogan, i) => {
+                        {this.state.slogans.map((slogan, i) => {
                             return this.renderSlogan(i, slogan);
                         })}
                     </div>
@@ -148,16 +231,17 @@ class Main extends React.Component {
                         {this.state.favorites.map((slogan, i) => {
                             return this.renderFavorite(i, slogan);
                         })}
+                        <p>You currently have selected {this.state.favorites.length} slogans.</p>
                         {this.state.favorites.length > 0 ? (
                             this.state.favorites.length <= 10 ? (
-                                <button className="submitbtn" onClick={this.submitVote} style={{margin: "20px", backgroundColor: 'red'}}>
+                                <button className="submitbtn" onClick={this.submitVote} >
                                     Submit
                                 </button>
                             ) : (
                                 <h2>Too many slogans selected, please remove some to submit.</h2>
                             )
                         ): (
-                            <h2>Favorite some slogans</h2>
+                            <h2>Choose some slogans</h2>
                         ) }
                     </div>
                 </div>
